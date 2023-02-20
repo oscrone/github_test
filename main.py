babd34c32a37450de79dd1cf85caae58c6208720
oscrone <vicanti@interia.pl>
2023-02-20 12:03:25 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import openpyxl
+import django
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+total_value_per_supplier = {}
+prod_under_10 = {}
+# product_list.cell()
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+
+    # calculation of nm of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        print(f"Adding a new supplier: {supplier_name}")
+        products_per_supplier[supplier_name] = 1
+
+    # calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logig products with inv < 10
+    if inventory < 10:
+        prod_under_10[int(product_num)] = int(inventory)
+
+    # add value total lalalalalala kurwrrroooo
+    inventory_price.value = inventory * price
+
+inv_file.save("inventory_with_total_value.xlsx")
+
+print("""
+Testing GitHub Repository
+Test 0.0.0.1v
+""")
+
+print(products_per_supplier)
+print(total_value_per_supplier)
+print(prod_under_10)
+
